90dde341e1824e554bcea9ea29dd2efd50c7a3a9
oxyfly <oxyblade@gmail.com>
2020-04-24 13:17:53 +0300

    updated

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-23a986c89e1db5c6875c.js"
+    "url": "webpack-runtime-88110b7752292d369122.js"
   },
   {
-    "url": "framework-3321703c33570853116c.js"
+    "url": "framework-8e210829bcc33c32949c.js"
   },
   {
-    "url": "app-66154d9ad69a0567b5e1.js"
+    "url": "app-99648925627831ead4e8.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9a0e48f39f1d3cdb944832abf7794af2"
+    "revision": "f6e3f65a44350f169db84185bf5091f2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
@@ -137,7 +137,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-66154d9ad69a0567b5e1.js`))) {
+  if (!resources || !(await caches.match(`/app-99648925627831ead4e8.js`))) {
     return await fetch(event.request)
   }
 
