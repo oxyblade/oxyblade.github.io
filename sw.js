1465d86643e81fc3e966006f6cb4f1de2bf6f083
oxyblade <oxyblade@gmail.com>
2020-06-10 21:51:24 +0100

    update

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-89913c891bc6855289d5.js"
+    "url": "webpack-runtime-d52a08e2c18d29e1c692.js"
   },
   {
     "url": "framework-84c9287a5714d2d8ce36.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2e7764e070877630681b0452c5a77ed1"
+    "revision": "2edcf01278c139ffe293823456b4f34b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
