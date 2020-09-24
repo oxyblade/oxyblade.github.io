9e3bdc90f6c2f17bba91d16886c70224e3d6ce66
oxyblade <oxyblade@gmail.com>
2020-09-24 22:39:46 +0300

    delete one project

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-97d8f4c37dd56e53e0fb.js"
+    "url": "webpack-runtime-d24ea160ac9eed6c2fcf.js"
   },
   {
     "url": "framework-84c9287a5714d2d8ce36.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b2cb946745b1d8eea3697637a8329eb2"
+    "revision": "08cc43be067609b1c5b741ac2f51bfa6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b1462017a2c34a95e2b4.js"
