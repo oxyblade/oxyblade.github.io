2df0c07ca8b59368af654ca9b24638f3524bb6ca
oxyblade <oxyblade@gmail.com>
2020-11-30 20:26:24 +0300

    improvements: header

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6ad417f9dbfe1cf87d0d.js"
+    "url": "webpack-runtime-49b4f511b3c380a88f07.js"
   },
   {
     "url": "framework-b17a64ad725e0b3c1338.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6ff74e726bd82f4c40c0d0c2eac66d32"
+    "revision": "ae54f592c9619cc268b88dffaebc5e8f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
